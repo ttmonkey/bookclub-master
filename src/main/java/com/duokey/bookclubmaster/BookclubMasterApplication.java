b6671a31e1faa987e783fab19968568dacb9306d
duoji <2977675402@qq.com>
2024-03-11 18:34:51 +0800

    Initial commit

---
+++ src/main/java/com/duokey/bookclubmaster/BookclubMasterApplication.java
@@ -0,0 +1,13 @@
+package com.duokey.bookclubmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookclubMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookclubMasterApplication.class, args);
+    }
+
+}
